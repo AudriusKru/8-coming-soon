abcf04684fe1146181845f8866ff15e3fc4d726b
AudriusKru <krutulis.audrius@gmail.cm>
2021-03-27 19:20:41 +0200

    video

--- js/components/matematika.js
+++ js/components/matematika.js
@@ -1,1 +1,19 @@
-const 
+const suma = (a, b) => a + b;
+const minus = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+const matematika = {
+    sudeti: suma,
+    atimti: minus,
+    sudauginti: multiply,
+    dalinti: divide
+
+}
+
+export {
+    suma,
+    minus,
+    multiply,
+    divide
+}
--- js/main-home.js
+++ js/main-home.js
@@ -13,13 +13,20 @@
 
 
 /* import ka from kur */
-import { suma } from './components/sum.js';
-
-
+ /* import { suma } from './components/sum.js';
+import { minus } from './components/minus.js';
+import { divide } from './components/divide.js';
 import { multiply } from './components/multiply.js';
+*/
+import { suma, minus, multiply, divide } from './components/matematika.js';  
+// sutraukta galima naudot bus maciau rasymo
 
 const rez1 = suma(8, 5);
-const rez2 = multiply(8, 5);
+const rez2 = minus(8, 5);
+const rez3 = divide(8, 5);
+const rez4 = multiply(8, 5);
 
 console.log(rez1);
 console.log(rez2);
+console.log(rez3);
+console.log(rez4);
